8399f75a002035a1045c3988b3014da59241e5e8
Joshua Robinson <13410890+patientsomeone@users.noreply.github.com>
2024-06-13 12:40:16 -0700

    Improved log messaging

--- bin/postbuild.sh
+++ bin/postbuild.sh
@@ -9,10 +9,14 @@ echo Patient Log: Recreating ./.amplify-hosting
 mkdir -p ./.amplify-hosting/compute
 
 echo Patient Log: Migrating to ./.amplify-hosting
+echo Patient Log: Default files
 cp -r ./dist ./.amplify-hosting/compute/default
+
+echo Patient Log: Node Modules
 cp -r ./node_modules ./.amplify-hosting/compute/default/node_modules
 
+echo Patient Log: Static files
 cp -r public ./.amplify-hosting/static
 
-echo Patient Log: migrating deploy-manifest.json
+echo Patient Log: deploy-manifest.json
 cp deploy-manifest.json ./.amplify-hosting/deploy-manifest.json
