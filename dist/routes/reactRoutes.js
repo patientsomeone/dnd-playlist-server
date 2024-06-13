43eb4f53526cd61dbbdef3d35067c1b3beceb8ee
Joshua Robinson <13410890+patientsomeone@users.noreply.github.com>
2024-06-13 11:19:51 -0700

    Removing ./dist

--- dist/routes/reactRoutes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.reactRoutes = void 0;
-const helloWorld_1 = require("../views/helloWorld");
-exports.reactRoutes = {
-    testReactRoutes: helloWorld_1.HelloWorld
-};
-//# sourceMappingURL=reactRoutes.js.map
