265bbf34c2af04623657cdb1b069a3dcf91f469e
Joshua Robinson <13410890+patientsomeone@users.noreply.github.com>
2024-06-13 12:41:03 -0700

    Reverted helloWorld.tsx to non-promise despite eslint's preference

--- src/views/helloWorld.tsx
+++ src/views/helloWorld.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import { Request } from "express";
 
-export const HelloWorld = (request: Request) => {
+export const HelloWorld = async (request: Request) => {
     console.log("Attempting to write to TSX");
-    return (
+    return Promise.resolve(
         <div>
             <h1> Hello World from the land of DND! </h1>
             <span>
