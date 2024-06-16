27984220b7e2d4b6e56ef81e71e7b7bf7defb620
Joshua Robinson <13410890+patientsomeone@users.noreply.github.com>
2024-06-15 23:15:09 -0700

    Reverting .amplify-hosting

--- bin/postbuild.sh
+++ bin/postbuild.sh
@@ -13,53 +13,40 @@ else
     echo AWS Build execution
 fi
 
-if ! cp ./deploy-manifest.json ./dist/deploy-manifest.json;
+echo Patient Log: Removing ./amplify-hosting
+if ! rm -rf ./.amplify-hosting;
 then
-    echo Failed to copy deploy-manifest
+    echo unable to remove ./.amplify-hosting
+fi
+
+echo Patient Log: Recreating ./.amplify-hosting
+if ! mkdir -p ./.amplify-hosting/compute;
+then
+    echo unable to create .amplify-hosting
+fi
+
+echo Patient Log: Migrating to ./.amplify-hosting
+echo Patient Log: Default files
+
+if ! mv ./dist ./.amplify-hosting/compute/default;
+then
+    echo Failed to move ./dist
     exit 1
 fi
 
-# echo Patient Log: Removing ./amplify-hosting
-# if ! rm -rf ./.amplify-hosting;
-# then
-#     echo unable to remove ./.amplify-hosting
-# fi
+echo Patient Log: Static files
+if ! mv public ./.amplify-hosting/static;
+then
+    echo Failed to move Static Files.
+    exit 1
+fi
 
-# echo Patient Log: Recreating ./.amplify-hosting
-# if ! mkdir -p ./.amplify-hosting/compute;
-# then
-#     echo unable to create .amplify-hosting
-# fi
+echo Patient Log: deploy-manifest.json
 
-# echo Patient Log: Migrating to ./.amplify-hosting
-# echo Patient Log: Default files
-
-# if ! mv ./dist ./.amplify-hosting/compute/default;
-# then
-#     echo Failed to move ./dist
-#     exit 1
-# fi
-
-# # echo Patient Log: Node Modules
-# # if ! mv ./node_modules ./.amplify-hosting/compute/default/node_modules;
-# # then
-# #     echo Failed to move ./node_modules
-# #     exit 1
-# # fi
-
-# echo Patient Log: Static files
-# if ! mv public ./.amplify-hosting/static;
-# then
-#     echo Failed to move Static Files.
-#     exit 1
-# fi
-
-# echo Patient Log: deploy-manifest.json
-
-# if ! mv deploy-manifest.json ./.amplify-hosting/deploy-manifest.json;
-# then
-#     echo Failed to move deploy-manifest.json
-#     exit 1
-# fi
+if ! mv deploy-manifest.json ./.amplify-hosting/deploy-manifest.json;
+then
+    echo Failed to move deploy-manifest.json
+    exit 1
+fi
 
 echo ----- PATIENT POST-BUILD LOGS END -----
