d4bf49838be24db055e7f2958acfa20802d144ea
Joshua Robinson <13410890+patientsomeone@users.noreply.github.com>
2024-06-15 19:03:48 -0700

    Created PROD vs DEV build check within postbuild

--- bin/postbuild.sh
+++ bin/postbuild.sh
@@ -2,6 +2,17 @@
 #!/bin/bash
 echo ----- PATIENT POST-BUILD LOGS BEGIN -----
 
+echo Evironment Check
+echo $AWS_APP_ID
+
+if [$AWS_APP_ID -eq ""];
+then
+    echo Local build execution, exiting
+    exit 0
+else
+    echo AWS Build execution
+fi
+
 echo Patient Log: Removing ./amplify-hosting
 if ! rm -rf ./.amplify-hosting;
 then
@@ -17,26 +28,26 @@ fi
 echo Patient Log: Migrating to ./.amplify-hosting
 echo Patient Log: Default files
 
-if ! cp -r ./dist ./.amplify-hosting/compute/default;
+if ! mv -r ./dist ./.amplify-hosting/compute/default;
 then
     echo Failed to copy ./dist
 fi
 
 echo Patient Log: Node Modules
-if ! cp -r ./node_modules ./.amplify-hosting/compute/default/node_modules;
+if ! mv -r ./node_modules ./.amplify-hosting/compute/default/node_modules;
 then
     echo Failed to copy ./node_modules
 fi
 
 echo Patient Log: Static files
-if ! cp -r public ./.amplify-hosting/static;
+if ! mv -r public ./.amplify-hosting/static;
 then
     echo Failed to copy Static Files.
 fi
 
 echo Patient Log: deploy-manifest.json
 
-if ! cp deploy-manifest.json ./.amplify-hosting/deploy-manifest.json;
+if ! mv deploy-manifest.json ./.amplify-hosting/deploy-manifest.json;
 then
     echo Failed to copy deploy-manifest.json
 fi
